13063a050f0de55273808662890303b0e7c64993
shander3377 <65767417+shander3377@users.noreply.github.com>
2023-03-26 16:59:05 +0530

    Add files via upload

---
+++ tables_debouncing.ino
@@ -0,0 +1,61 @@
+//  including ezbutton module (easy button module)
+#include<ezButton.h>
+
+//  connect both buttons to pin 4 and 22
+const int button1 = 22;
+const int button2 = 4;
+
+//  defining a variable to track count of button press
+int count = 0;
+
+//  creating button objects
+ezButton push_button1(button1);
+ezButton push_button2(button2);
+
+void setup()
+{
+  //  change the baud rate to 9600 if required
+  Serial.begin(115200);
+  Serial.println("Started!");
+  //  set debounce time for both the button objects
+  push_button1.setDebounceTime(50);
+  push_button2.setDebounceTime(50);
+
+}
+
+void loop()
+{
+  //  loop both the push_buttons : continuous polling
+  push_button2.loop();
+  push_button1.loop();
+
+
+
+  //  check if push_button1 is pressed : store the button push count into count variable
+  if (push_button1.isPressed())
+  {
+    count=count+1;
+    Serial.println(count);
+  }
+
+  //  else if push_button2 is pressed : print the table of the number stored in variable count
+  else if (push_button2.isPressed())
+  {
+    Serial.print("Number: ");
+    Serial.println(count);
+    Serial.print("Table of " );
+    Serial.println(count);
+    //  draft a loop to print the table
+for(int i=1; i<11; i++){
+  Serial.print(count);
+  Serial.print("X");
+  Serial.print(i);
+  Serial.print("=");
+  Serial.println(count*i);
+}
+
+    //  reset the number to 0, once table is printed
+    count=0;
+  }
+ 
+}
